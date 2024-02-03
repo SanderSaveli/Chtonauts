104118aac4b3d978c79364928932fad9c8491c6c
Darkkness10 <Darkknesss10@gmail.com>
2024-02-03 12:31:48 +0300

    Update cameraController.cs

--- Assets/Scripts/Input/cameraController.cs
+++ Assets/Scripts/Input/cameraController.cs
@@ -4,25 +4,33 @@ using UnityEngine;
 
 public class cameraController : MonoBehaviour
 {
-     public float speed = 5.0f;
-void Update()
-{
-   
-	if(Input.GetKey(KeyCode.D))
-	{
-		transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
-	}
-	if(Input.GetKey(KeyCode.A))
-	{
-		transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
-	}
-	if(Input.GetKey(KeyCode.S))
-	{
-		transform.Translate(new Vector3(0,-speed * Time.deltaTime,-speed * Time.deltaTime));
-	}
-	if(Input.GetKey(KeyCode.W))
-	{
-		transform.Translate(new Vector3(0,speed * Time.deltaTime,speed * Time.deltaTime));
-	}
-}
+    public float speed = 5.0f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+
+        
+        transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
+    }
 }
