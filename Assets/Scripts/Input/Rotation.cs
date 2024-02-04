8492e0cc2e1d3f7cdc82d3ba8d3a81ea5446ff0d
Darkkness10 <Darkknesss10@gmail.com>
2024-02-04 11:28:35 +0300

    04.02.24

--- Assets/Scripts/Input/Rotation.cs
+++ Assets/Scripts/Input/Rotation.cs
@@ -4,24 +4,45 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
-   public Transform Target;
-   public float MouseSens = 2.5f;
-   public  float timer = 0;
+    public float MouseSens = 2.5f;
+    public float timer = 0;
+    private Vector3 center;
+
+    void Start()
+    {
+        // Получаем все рендереры дочерних объектов
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+
+        // Если есть рендереры, вычисляем центр их объединения
+        if (renderers.Length > 0)
+        {
+            Bounds bounds = renderers[0].bounds;
+            for (int i = 1; i < renderers.Length; i++)
+            {
+                bounds.Encapsulate(renderers[i].bounds);
+            }
+            center = bounds.center;
+        }
+        else
+        {
+            // Если рендереров нет, используем позицию объекта
+            center = transform.position;
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-       if(Input.GetMouseButton(1))
-       {
-        timer -=Time.deltaTime;
-        if(timer<=0)
+        if (Input.GetMouseButton(1))
         {
-            float hor = Input.GetAxis("Mouse X");
-            float Ver = Input.GetAxis("Mouse Y");
-            transform.RotateAround(Target.position,Vector3.up , hor *MouseSens *300 * Time.deltaTime);
-
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                float hor = Input.GetAxis("Mouse X");
+                // float ver = Input.GetAxis("Mouse Y"); // Эта строка больше не нужна
+                transform.RotateAround(center, Vector3.up, hor * MouseSens * 300 * Time.deltaTime);
+                // Вращение вокруг оси Y удалено
+            }
         }
-       } 
-       
     }
 }
---
+++ Assets/Scripts/tile/NewSurfaceShader.shader
@@ -0,0 +1,64 @@
+Shader "Custom/NewSurfaceShader"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _OutlineColor ("Outline Color", Color) = (1,1,1,1)
+        _OutlineWidth ("Outline Width", Range(0.0, 0.1)) = 0.01
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            fixed4 _OutlineColor;
+            float _OutlineWidth;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // Sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv);
+
+                // Calculate the outline
+                if (i.uv.x < _OutlineWidth || i.uv.x > 1 - _OutlineWidth ||
+                    i.uv.y < _OutlineWidth || i.uv.y > 1 - _OutlineWidth)
+                {
+                    col = _OutlineColor;
+                }
+
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
